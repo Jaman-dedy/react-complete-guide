3422b37921db898075c4e8a87aae02a7e5cc3aa2
Jaman Dedy <jeandedieuam@gmail.com>
2019-11-15 10:05:00 +0200

    ref with hooks`

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,31 +1,35 @@
-import React from 'react';
-import classes from './Cockpit.module.css';
+import React, { useEffect, useRef } from "react";
+import classes from "./Cockpit.module.css";
 
+const Cockpit = props => {
+  const toggleBtnRef = useRef(null);
 
-const Cockpit = (props) => {
+  useEffect(() => {
+    toggleBtnRef.current.click();
+  }, []);
 
-    let assignedClasses = [];
-    let btnClass = '';
-    if(props.shoPersons){
-        btnClass = classes.red;
-    }
-    
-    if(props.persons.length <= 2){
-      assignedClasses.push(classes.red); 
-    }
-    if(props.persons.length <= 1){
-      assignedClasses.push(classes.bold);
-    }
+  let assignedClasses = [];
+  let btnClass = "";
+  if (props.shoPersons) {
+    btnClass = classes.red;
+  }
 
-    return (
-        <div className={classes.Cockpit}>
-        <h1> hi, I'm a react app</h1>
-        <p className={assignedClasses.join(' ')}>This is really working!</p>
-        <button
-        className= {btnClass} 
-        onClick={props.toggle}>Toggle persons</button>
-        </div>
-    )
-}
+  if (props.persons.length <= 2) {
+    assignedClasses.push(classes.red);
+  }
+  if (props.persons.length <= 1) {
+    assignedClasses.push(classes.bold);
+  }
+
+  return (
+    <div className={classes.Cockpit}>
+      <h1> hi, I'm a react app</h1>
+      <p className={assignedClasses.join(" ")}>This is really working!</p>
+      <button ref={toggleBtnRef} className={btnClass} onClick={props.toggle}>
+        Toggle persons
+      </button>
+    </div>
+  );
+};
 
 export default Cockpit;
