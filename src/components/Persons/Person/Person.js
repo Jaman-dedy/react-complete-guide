744ce897f31d4886f2d321cb29c4b0938adeaed5
Jaman Dedy <jeandedieuam@gmail.com>
2019-11-15 09:58:17 +0200

    using refs`

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,25 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 import classes from "./Person.module.css";
 import withClass from "../../../hoc/withClass";
 import Aux from "../../../hoc/Aux";
 
-const person = props => {
-  return (
-    <Aux>
-      <p onClick={props.click}>
-        I'm a {props.name} and I am {props.age} years old!
-      </p>
-      <p>{props.children}</p>
-      <input type="text" onChange={props.changed} value={props.name} />
-    </Aux>
-  );
-};
+class Person extends Component {
+  constructor(props) {
+    super(props);
+    this.inputElementRef = React.createRef();
+  }
+  componentDidMount() {
+    // this.inputElement.focus();
+    this.inputElementRef.current.focus();
+  }
+  render() {
+    return (
+      <Aux>
+        <p onClick={this.props.click}>
+          I'm a {this.props.name} and I am {this.props.age} years old!
+        </p>
+        <p>{this.props.children}</p>
+        <input
+          type="text"
+          // ref={inputEl => {
+          //   this.inputElement = inputEl;
+          // }}
+          ref={this.inputElementRef}
+          onChange={this.props.changed}
+          value={this.props.name}
+        />
+      </Aux>
+    );
+  }
+}
 
 // Person.PropTypes = {
 //   click: PropTypes.func,
 //   name: PropTypes.string,
 //   age: PropTypes.number
 // }
-export default withClass(person, classes.Person);
+export default withClass(Person, classes.Person);
